9dbaa2b9834369b31466234fdcd1bf7fb8847bb7
s.a. faroque <faroque.computer@gmail.com>
2023-08-14 10:21:25 +0600

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="container mx-auto">
+        <div class="flex gap-5">
+            <div class="w-3\/4 bg-gray-500">
+                <nav>
+                    <li class="list-none"> <a class="p-2 block" href="#"> Clients </a> </li>
+                    <li class="list-none"> <a class="p-2 block" href="#"> Clients </a> </li>
+                    <li class="list-none"> <a class="p-2 block" href="#"> Clients </a> </li>
+                    <li class="list-none"> <a class="p-2 block" href="#"> Clients </a> </li>
+                    <li class="list-none"> <a class="p-2 block" href="#"> Clients </a> </li>
+                    
+                </nav>
+            </div>
+            <div class="w-5/6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti numquam, quo unde optio iusto quae totam, laborum cupiditate consectetur saepe odit exercitationem pariatur, eaque ex esse modi provident cumque repellat a magnam nostrum rem. Perspiciatis id voluptatum illo voluptates autem distinctio impedit doloremque quibusdam accusantium molestias amet quos, porro ad veritatis, saepe molestiae dolorem incidunt unde assumenda voluptate alias quae. Animi sunt asperiores voluptate explicabo consequatur sit provident, deserunt ullam id voluptas alias architecto molestias corporis vero facere aut labore fugit consequuntur. Error maxime, ipsam, molestiae magni ut aperiam iusto nihil placeat minima enim est assumenda modi veniam officiis architecto!
+            </div>
+        </div>
+    </div>
+
+    
+</x-app-layout>
